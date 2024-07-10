79e46a64c5e85c4242df748aa198436fd67412a4
avik <github.maturity983@passinbox.com>
2024-07-10 21:12:06 +0530

    feat: added functions to Decode ABI-encoded input &  ABI encode the given function arguments

--- src/ape_utils/utils.py
+++ src/ape_utils/utils.py
@@ -45,14 +45,79 @@ def call_view_function(function_sig: str, address: str, args: int) -> Any:
     return output
 
 
-def encode_calldata_using_ape(signature: str, *args: Any) -> Union[HexBytes, Any]:
+def encode_calldata(signature: str, *args: Any) -> Union[HexBytes, Any]:
+    """
+    Encodes calldata for a function given its signature and arguments using Ape.
+
+    This function takes a function signature and a variable number of arguments,
+    parses the function's ABI (Application Binary Interface), and encodes the
+    calldata for making a blockchain transaction or call.
+
+    Args:
+        signature (str): The function signature in the format "function_name(input1_type,input2_type,...)".
+        *args (Any): The arguments to be encoded for the function call. The number and types of arguments must match
+                     the function signature.
+
+    Returns:
+        Union[HexBytes, Any]: The encoded calldata in hex bytes format, which can be used for making a blockchain
+                              transaction or call.
+
+    Raises:
+        ValueError: If the number of provided arguments does not match the expected number of inputs according to the
+                    function signature.
+
+    Example:
+        >>> encode_calldata("transfer(address,uint256)", "0xRecipientAddress", 1000)
+        HexBytes('0xa9059cbb000000000000000000000000RecipientAddress0000000000000000000000000000000000000000000000000000000000000000000000000000000000003e8')
+
+    Notes:
+        - The function signature should match the Solidity function format.
+        - The arguments should be provided in the correct order and of the correct type as specified in the function signature.
+
+    """  # noqa: E501
     method_abi: MethodABI = ethpm_types.abi.MethodABI.from_signature(signature)
-    return ape.networks.ethereum.encode_calldata(method_abi, args)
+    console.print(f"{args=}")
+    if len(method_abi.inputs) != len(args):
+        msg = f"Wrong number of parameters passed. Expected: {len(method_abi.inputs)} got: {len(args)}"
+        raise ValueError(msg)
+    return ape.networks.ethereum.encode_calldata(method_abi, *args)
+
+
+def decode_calldata(signature: str, encodeded_data: str) -> Union[dict, Any]:
+    """
+    Decodes calldata for a function given its signature and calldata string using Ape.
+
+    This function takes a function signature and a calldata string,
+    parses the function's ABI (Application Binary Interface), and decodes the
+    calldata to its original arguments.
+
+    Args:
+        signature (str): The function signature in the format "function_name(input1_type,input2_type,...)".
+        calldata (str): The calldata string to be decoded.
+
+    Returns:
+        Union[dict, Any]: The decoded arguments as a dictionary or any appropriate type based on the function signature.
+
+    Raises:
+        ValueError: If the calldata is not properly formatted or if the signature is incorrect.
+
+    Example:
+        >>> decode_calldata_using_ape("transfer(address,uint256)", "0xa9059cbb000000000000000000000000RecipientAddress0000000000000000000000000000000000000000000000000000000000000000000000000000000000003e8")
+        {'recipient': '0xRecipientAddress', 'amount': 1000}
+
+    Notes:
+        - The function signature should match the Solidity function format.
+        - The calldata string should be in hex format.
+
+    """  # noqa: E501
+    method_abi: MethodABI = ethpm_types.abi.MethodABI.from_signature(signature)
+    encoded_data_bytes: bytes = bytes.fromhex(encodeded_data[2:])
+    return ape.networks.ethereum.decode_calldata(method_abi, encoded_data_bytes)
 
 
 if __name__ == "__main__":
-    function_sig: str = "gsr_query(uint256)(string)"
-    address: str = "0x9b7FD6FF5e427F8470E1da652f21A79Bed318f38"
+    function_sig: str = "call_this_view_function(uint256)(string)"
+    address: str = "0x80E097a70cacA11EB71B6401FB12D48A1A61Ef54"
     args = 6147190
 
     call_view_function(function_sig, address, args)
